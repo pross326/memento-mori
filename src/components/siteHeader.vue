737d532542a68952377059a5f0114b2b10495d6d
Paul Ross <pross326@gmail.com>
2022-11-21 10:15:11 -0500

    add vuetify

---
+++ src/components/siteHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar
+  elevation="4"
+>akufgdhlakgufn</v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'siteHeader',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/siteHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <v-card>
+          HELLO
+      </v-card>
+
+      <v-btn
+  elevation="2"
+  outlined
+></v-btn>
+      </div>
+</template>
+
+<script>
+
+export default {
+    name: 'siteHome',
+}
+</script>
+
+<style>
+
+</style>
