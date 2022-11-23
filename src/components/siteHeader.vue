ba6cc95b748e1d01e752d7e14ec5027eb27eaf64
TELEFLEX\pross1 <paul.ross@teleflex.com>
2022-11-23 17:06:43 -0500

    set up UI

--- src/components/siteHeader.vue
+++ src/components/siteHeader.vue
@@ -1,13 +1,28 @@
 <template>
-  <v-app-bar
-  elevation="4"
->akufgdhlakgufn</v-app-bar>
+  <div>
+    <v-toolbar
+      dark
+      rounded
+      outlined
+      prominent
+      height="150px"
+      
+      src="https://wallpapercave.com/wp/wp4999049.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-h2 font-weight-bold" >Memento Mori</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'siteHeader',
-}
+  name: "siteHeader",
+};
 </script>
 
 <style>
