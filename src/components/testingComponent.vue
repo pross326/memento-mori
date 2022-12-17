89f0c57009f09cb984276f13ead8562936cd21fb
TELEFLEX\pross1 <paul.ross@teleflex.com>
2022-12-16 20:59:25 -0500

    hopefully this isn't broken

---
+++ src/components/testingComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <enterData />
+    <displayData @pass-data="handleEvent($event)" :picker="this.picker"/>
+  </div>
+</template>
+
+<script>
+import enterData from "./enterData.vue";
+import displayData from "./displayData.vue";
+
+export default {
+  name: "testingComponent",
+  components: { enterData, displayData },
+  data: () => ({
+    //
+  }),
+}
+
+
+</script>
+
+<style></style>
