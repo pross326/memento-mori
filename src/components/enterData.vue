63b5d2bebe9cba585d232d4f431d22403a7c2006
TELEFLEX\pross1 <paul.ross@teleflex.com>
2022-12-21 22:22:55 -0500

    UI updates

--- src/components/enterData.vue
+++ src/components/enterData.vue
@@ -1,60 +1,71 @@
 <template>
   <div>
-    <v-row class="justify-center pt-5 pb-3 grey darken-4">
-      <v-card class="text-center justify-center d-flex align-center flex-column col-md-8">
-      <v-card-title class="text-h4">
-        Living a Life of Purpose and Gratitude
-      </v-card-title>
-      <v-card-text>
-        Memento mori is a Latin phrase that means "remember that you have to die." It is a reminder to embrace the finite nature of life and to make the most of every moment.
-Memento mori serves as a reminder that death is inevitable, and that we should not get too attached to material possessions or earthly success. Instead, we should focus on what truly matters in life, such as relationships, personal growth, and making a positive impact on the world.
-By embracing memento mori, we can live our lives with a sense of purpose, gratitude, and resilience, and make the most of every day.
-      </v-card-text>
-    </v-card>
+    <v-row class="justify-center pt-5 pb-3 grey darken-4 ">
+      <v-card class="col-xs-10 col-sm-10 col-xl-8 justify-center align-center d-flex flex-column text-center pa-2 ma-2">
+        <v-card-title class="text-h4 fw-bolder">
+          Living a Life of Purpose and Gratitude
+        </v-card-title>
+        <v-card-text class="">
+          Memento mori is a Latin phrase that means "remember that you have to
+          die." It is a reminder to embrace the finite nature of life and to
+          make the most of every moment. Memento mori serves as a reminder that we should focus on
+          what truly matters in life, such as relationships, personal growth,
+          and making a positive impact on the world. By embracing memento mori,
+          we can live our lives with a sense of purpose, gratitude, and
+          resilience, and make the most of every day.
+        </v-card-text>
+        <v-spacer></v-spacer>
+        <v-card-text>
+          Enter your birth date and life expectancy below for a visual reminder to live each day to the fullest. 
+        </v-card-text>
+      </v-card>
     </v-row>
-  <v-container class="grey darken-4" fluid height="fill">
-    <v-row class="ma-1">
-      <v-spacer></v-spacer>
-      <v-col cols="10">
-        <v-stepper v-model="e1" height="750">
+    <v-container class="grey darken-4 " fluid height="fill">
+      <v-row class="ma-1">
+        <v-spacer></v-spacer>
+        <v-stepper v-model="e1" class="col-md-8 col-xs-12">
           <v-stepper-header>
             <v-spacer></v-spacer>
             <v-stepper-step :complete="e1 > 1" step="1">
-              Enter Your Birthday
+              Date of Birth
             </v-stepper-step>
             <v-divider></v-divider>
             <v-stepper-step :complete="e1 > 2" step="2">
-              Enter Your Life Expectancy
+              Life Expectancy
             </v-stepper-step>
             <v-spacer></v-spacer>
           </v-stepper-header>
           <v-stepper-items>
-            <v-stepper-content step="1">
-              <v-card class="ma-10" color="grey lighten-0">
-                <v-card-title class="text-h4">
-                  Enter your birthday.
-                </v-card-title>
-                <v-row>
-                  <v-col class="d-flex justify-center">
-                    <v-date-picker
-                      class="d-flex justify-center rounded-lg mb-10 col-md-6 col-xl-8"
-                      @input="picker"
-                      v-model="formData.birthDate"
-                    ></v-date-picker>
-                  </v-col>
-                </v-row>
+            <v-stepper-content step="1" class="align-items">
+              <v-card height="fill" color="grey darken-2" class="d-flex flex-column align-center mb-5">
+                <v-card elevation="4"
+                  class=" col-xs-10 col-sm-10 col-xl-5 justify-center align-center d-flex flex-column pa-0 mt-3">
+                  <v-card-title class="text-h5">
+                    Enter your birth date.
+                  </v-card-title>
+                  <v-card-subtitle>
+                    You birth date is used to calculate how many weeks you've
+                    lived.
+                  </v-card-subtitle>
+                </v-card>
+                  <v-date-picker elevation="4"
+                    class="d-flex justify-center rounded-lg mb-3 mt-3 col-xs-10 col-sm-10 col-xl-5" @input="picker"
+                    v-model="formData.birthDate"></v-date-picker>
               </v-card>
               <div class="d-flex justify-center">
-                <v-btn color="primary" @click="e1 = 2; emitEvent()"> Continue </v-btn>
+                <v-btn :disabled="dateIsValid" color="primary" @click="e1 = 2">
+                  Continue
+                </v-btn>
               </div>
             </v-stepper-content>
-            <v-stepper-content step="2">
-              <v-card class="ma-12" color="grey lighten-0">
+            <v-stepper-content step="2" class="align-items">
+              <v-card height="fill" color="grey darken-2" class="d-flex flex-column align-center mb-5">
                 <v-card-title class="text-h4">
                   Enter your Life Expectancy.
                 </v-card-title>
                 <v-card-subtitle> (80 Default) </v-card-subtitle>
-                <v-row>
+                </v-card>
+                <v-card>
                   <v-spacer></v-spacer>
                   <v-col cols="8" class="justify-center mb-10">
                     <v-card elevation="5">
@@ -62,26 +73,19 @@ By embracing memento mori, we can live our lives with a sense of purpose, gratit
                         {{ formData.lifeExpectancy }} Years
                       </v-card-title>
                       <v-card-text>
-                        <v-slider
-                          v-model="formData.lifeExpectancy"
-                          height="150"
-                          step="10"
-                          min="60"
-                          thumb-label
-                          ticks
-                          class="d-flex justify-center rounded-lg col-md-6 col-xl-8"
-                        ></v-slider>
-                        <v-card-subtitle class="d-flex justify-center"
-                          >Use the slider above</v-card-subtitle
-                        >
+                        <v-slider v-model="formData.lifeExpectancy" height="150" step="10" min="60" thumb-label ticks
+                          class="d-flex justify-center rounded-lg col-md-6 col-xl-8"></v-slider>
+                        <v-card-subtitle class="d-flex justify-center">Use the slider above</v-card-subtitle>
                       </v-card-text>
                     </v-card>
                   </v-col>
                   <v-spacer></v-spacer>
-                </v-row>
-              </v-card>
+                </v-card>
               <div class="d-flex justify-center">
-                <v-btn color="primary" class="ma-5" @click="switchComponent(); $emit('handleSubmit', formData)">
+                <v-btn color="primary" class="ma-5" @click="
+                  switchComponent();
+                $emit('handleSubmit', formData);
+                ">
                   Continue
                 </v-btn>
                 <v-btn text class="ma-5" @click="e1 = 1"> Go Back </v-btn>
@@ -89,10 +93,9 @@ By embracing memento mori, we can live our lives with a sense of purpose, gratit
             </v-stepper-content>
           </v-stepper-items>
         </v-stepper>
-      </v-col>
-      <v-spacer></v-spacer>
-    </v-row>
-  </v-container>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -101,17 +104,40 @@ export default {
   name: "enterData",
   data: () => {
     return {
-    picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-      .toISOString()
-      .substr(0, 10),
-    e1: 1,
-    formData: {
-      birthDate: '',
-      lifeExpectancy: 80,
-    }
-  }},
+      picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      e1: 1,
+      steps: [
+        {
+          id: 1,
+          title: 'Enter Your Birth Date',
+          subtitle: "Your birth date is used to calculate how many weeks you've lived.",
+          input: ''
+        },
+        {
+          id: 2,
+          title: 'Enter Your Life Expectancyt=',
+          subtitle: "(80 Default)",
+          input: ''
+        }
+      ],
+      formData: {
+        birthDate: "",
+        lifeExpectancy: 80,
+      },
+    };
+  },
   props: ["switch-component"],
-
+  computed: {
+    dateIsValid() {
+      if (this.formData.birthDate == "") {
+        return true;
+      } else {
+        return this.formData.birthDate >= this.picker;
+      }
+    },
+  },
 };
 </script>
 
